d4d7fae6ab61a4d6e8bf66d2cfc43f25a364862e
Jose Irizarry <jose@mrkp.co>
2020-06-24 18:14:47 -0400

    fix google chat post

--- app/jobs/google_chat_job.rb
+++ app/jobs/google_chat_job.rb
@@ -3,14 +3,14 @@ class GoogleChatJob < ActiveJob::Base
 
   def perform(post)
     # Assemble message
-    url = "#{AppSettings['settings.site_url']}#{Rails.application.routes.url_helpers.admin_topic_path(post.topic.id)}"
+    topic_url = "#{AppSettings['settings.site_url']}#{Rails.application.routes.url_helpers.admin_topic_path(post.topic.id)}"
     title = "New Topic started by #{post.topic.user.name}"
     message_headers = {'Content-Type' => 'application/json; charset=UTF-8'}
     message = {
-      text: "#{title} \n #{post.body}"
+      text: "#{title} \n #{topic_url} \n #{post.body}"
     }
 
     # Send Ping
-    HTTP.post(url, headers: message_headers, body: message.to_json)
+    HTTP.post(AppSettings['google_chat.post_url'], headers: message_headers, body: message.to_json)
   end
 end
